f97ad60cc85512ccb0adedfd4438310e5be9230e
hrjayasuryasingh9 <hrjayasuryasingh@gmail.com>
2025-07-21 19:34:25 +0530

    first commit

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:3005/api"
-      : "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
+
